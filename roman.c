30a2129dae5c9c00da729aa050e7db18cd13f490
sowbarnigavsb <31504504+sowbarnigavsb@users.noreply.github.com>
2018-03-16 11:31:04 +0000

    Create roman.c

---
+++ roman.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+int main(void) {
+	char ch[40];
+            scanf("%s",ch);
+	switch(ch)
+	{
+	case "I":
+	            printf("1");
+		break;
+	case "II":
+		printf("2");
+		break;
+	case "III":
+		printf("3");
+		break;
+	case "IV":
+		printf("4");
+		break;
+	case "V":
+		printf("5");
+		break;
+	case "VI":
+		printf("6");
+		break;
+	case "VII":
+		printf("7");
+	            break;
+            case "VIII":
+		printf("8");
+		break; 
+	case "IX":
+		printf("9");
+		break;
+	case "X":
+		printf("10");
+		break;
+	case "XI":
+		printf("11");
+		break;
+	case "XII":
+		printf("12");
+		break;
+	case "XIII":
+		printf("13");
+		break;
+	case "XIV":
+		printf("14");
+		break;
+	case "XV":
+		printf("15");
+	            break;
+	case "XVI":
+		printf("16");
+	            break;
+	case "XVII":
+		printf("17");
+	            break;
+	case "XVIII":
+		printf("18");
+	            break;
+	case "XIX":
+		printf("19");
+	            break;
+	case "XX":
+		printf("20");
+	            break;
+	default:
+	            printf("Invalid Input");
+	}
+	// your code goes here
+	return 0;
+}
